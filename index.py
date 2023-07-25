3c650d30fb7466338198c8f4a55810420a6eb494
azraelvayne <65314654+azraelvayne@users.noreply.github.com>
2023-07-25 08:52:46 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,58 +1,55 @@
-import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from api.chatgpt import ChatGPT
+
+import os
+
+line_bot_api = LineBotApi(os.getenv("LEPQ+PdKPSLcoS9GBbdFukdoHl+KigQAE1/6atOcvQn97Own17jB19NydaKRrEuMNR0c0P2h9jFk7IVWb3DQs/cEJGvVFGF6Ki/v+Nw8+QWGTYpeEkk9v7Yb6/OCR0OS7RmAlJWhRW42/a6 kmbQiMgdB04t89/1O/w1cDnyilFU="))
+line_handler = WebhookHandler(os.getenv("1268eb4f16e5d543a622a4c4341abbd6"))
+working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 app = Flask(__name__)
+chatgpt = ChatGPT()
 
-line_bot_api = LineBotApi(os.environ['LEPQ+PdKPSLcoS9GBbdFukdoHl+KigQAE1/6atOcvQn97Own17jB19NydaKRrEuMNR0c0P2h9jFk7IVWb3DQs/cEJGvVFGF6Ki/v+Nw8+QWGTYpeEkk9v7Yb6/OCR0OS7RmAlJWhRW42/a6 kmbQiMgdB04t89/1O/w1cDnyilFU='])
-handler = WebhookHandler(os.environ['1268eb4f16e5d543a622a4c4341abbd6'])
-
-import openai
-
-openai.api_key = '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'
-
+# domain root
+@app.route('/')
+def home():
+    return 'Hello, World!'
 
 @app.route("/webhook", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    if not body:
-        abort(400)
-
+    # handle webhook body
     try:
-        handler.handle(body, signature)
+        line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+    return 'OK'
 
-    return 'OK',200
 
-
-@handler.add(MessageEvent, message=TextMessage)
+@line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    user_message = event.message.text
+    global working_status
+    working_status = True
+    
+    if event.message.type != "text":
+        return
+    
 
-    # 使用ChatGPT進行回應生成
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Hi."},
-            {"role": "user", "content": user_message}
-        ]
-    )
-
-    # 從回應物件中取得ChatGPT生成的回應
-    chat_reply = response.choices[0].message.content
-
-    # 回傳ChatGPT生成的回應給LINE BOT的用戶
-    message = TextSendMessage(text=chat_reply)
-    line_bot_api.reply_message(event.reply_token, message)
+    if working_status:
+        chatgpt.add_msg(f"Human:{event.message.text}?\n")
+        reply_msg = chatgpt.get_response().replace("AI:", "", 1)
+        chatgpt.add_msg(f"AI:{reply_msg}\n")
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-    
+    app.run()
